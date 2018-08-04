629dbd536a70496178bf49bc6a67d4b20bf18ff7
Andrey Rybachenko <arybachenko@kaaiot.io>
2018-08-04 19:07:56 +0300

    Add logik for Player Search Helper Map

--- src/main/java/org/rib/gamemap/core/PlayersMappingHelper.java
+++ src/main/java/org/rib/gamemap/core/PlayersMappingHelper.java
@@ -1,13 +1,18 @@
 package org.rib.gamemap.core;
 
 import org.rib.gamemap.model.Coordinates;
+import org.rib.gamemap.model.Player;
 import org.springframework.stereotype.Component;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.rib.gamemap.common.Constants.Game.FIELD_SIZE;
+import static org.rib.gamemap.common.Constants.Game.LOOK_SIZE;
 import static org.rib.gamemap.common.Constants.Game.PLAYERS_COUNT;
 
 @Component
@@ -15,10 +20,35 @@ public class PlayersMappingHelper {
 
     private static final int SECTION_SIZE = FIELD_SIZE;
 
-    private Map<Coordinates, List<Integer>> playersByMapSecvtions = new HashMap<>(PLAYERS_COUNT);
+    private Map<Coordinates, Set<Integer>> playersByMapSections = new HashMap<>(PLAYERS_COUNT);
 
     public PlayersMappingHelper() {
-        int mapSectionsX = (FIELD_SIZE + SECTION_SIZE - 1) / SECTION_SIZE;
-        playersByMapSecvtions = new HashMap<>(PLAYERS_COUNT)
+        int mapHeightInSections = (FIELD_SIZE + SECTION_SIZE - 1) / SECTION_SIZE;
+        playersByMapSections = new HashMap<>(mapHeightInSections * mapHeightInSections)
     }
+
+    public void addPlayer(Player player, int playerX, int playerY) {
+        int sectionX = playerX / SECTION_SIZE;
+        int sectionY = playerY / SECTION_SIZE;
+
+        Coordinates sectionCoordinates = new Coordinates(sectionX, sectionY);
+        playersByMapSections
+                .computeIfAbsent(sectionCoordinates, (coord) -> new HashSet<>())
+                .add(player.getId());
+    }
+
+
+    public List<Integer> getPlayersIdForSector(int sectorX, int sectorY) {
+        int startSectionX = sectorX / SECTION_SIZE;
+        int startSectionY = sectorY / SECTION_SIZE;
+        int endSectionX = (sectorX + LOOK_SIZE) / SECTION_SIZE;
+        int endSectionY = (sectorY + LOOK_SIZE) / SECTION_SIZE;
+
+        return getPlayersIds(startSectionX, startSectionY, endSectionX, endSectionY);
+    }
+
+    private List<Integer> getPlayersIds(final int startSectionX, final int startSectionY, final int endSectionX, final int endSectionY) {
+        throw new NotImplementedException();
+    }
+
 }
